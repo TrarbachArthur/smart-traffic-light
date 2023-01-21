4c49a1b3a7765d5fd2447491a62f6762a78d8c2c
TrarbachArthur <arthurtrarbach.sampaio@gmail.com>
2023-01-21 12:32:50 -0300

    Delete unused code

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 
 const int PED_RED = 6;
 const int PED_GREEN = 5;
-const int MAIN_RED = 4;
+const int MAIN_RED =4;
 const int MAIN_YELLOW = 3;
 const int MAIN_GREEN = 2;
 const int CYCLES_BLINK = 6;
@@ -18,7 +18,8 @@ unsigned long arrivalPed;
 unsigned long latestCar;
 
 void handleChecks() {
-	if(analogRead(PED_PIEZO) > 900 && !waitingPed && millis() - latestCross > 3000) {
+	if(analogRead(PED_PIEZO) > 1000 && !waitingPed && millis() - latestCross > 3000) {
+    Serial.println("Pedestre detectado.");
 		waitingPed = true;
 		arrivalPed = millis();
 	}
@@ -78,9 +79,7 @@ void setup() {
 
 	Serial.println("Inicializando...");
 	Serial.println("Por favor aguarde.");
-	while (analogRead(CAR_PIEZO) > 800 || analogRead(PED_PIEZO) > 800) {
-
-	}
+  Serial.println("Inicializacao concluida com sucesso!");
 
 	Timer1.initialize(100000);
 	Timer1.attachInterrupt(handleChecks);
@@ -110,98 +109,3 @@ void loop() {
 		} 
 	}
 }
-
-/*
-const int PED_RED = 2;
-const int PED_GREEN = 3;
-const int MAIN_RED = 4;
-const int MAIN_YELLOW = 5;
-const int MAIN_GREEN = 6;
-const int CYCLES_BLINK = 6;
-volatile bool red = false;
-volatile bool green = false;
-volatile bool waitingPed = false;
-unsigned long arrivalPed;
-unsigned long latestCar;
-
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(PED_RED, OUTPUT);
-  pinMode(PED_GREEN, OUTPUT);
-  pinMode(MAIN_GREEN, OUTPUT);
-  pinMode(MAIN_YELLOW, OUTPUT);
-  pinMode(MAIN_GREEN, OUTPUT);
-  digitalWrite(PED_GREEN, LOW);
-  digitalWrite(PED_RED, LOW);
-  digitalWrite(MAIN_RED, LOW);
-  digitalWrite(MAIN_YELLOW, LOW);
-  digitalWrite(MAIN_GREEN, LOW);
-  
-  Serial.begin(9600);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  
-  turnRed();
-  delay(3000);
-  turnGreen();
-  delay(5000);
-  
-  if (!green) {
-  	turnGreen();
-    green = true;
-    red = false;
-  }
-
-  Serial.println(analogRead(CAR_PIEZO));
-  
-  if(analogRead(PED_PIEZO) > 450 && !waitingPed) {
-    waitingPed = true;
-	arrivalPed = millis();
-  }
-  
-  if(analogRead(CAR_PIEZO) > 450) {
-  	latestCar = millis();
-  }
-  
-  if (waitingPed && (arrivalPed - latestCar > 5000 || arrivalPed - millis() > 8000)) {
-  	turnRed();
-    red = true;
-    green = false;
-    waitingPed = false;
-  }
-}
-
-void turnRed() {
-  digitalWrite(MAIN_GREEN, LOW);
-  digitalWrite(MAIN_YELLOW, HIGH);
-  delay(1500);
-  digitalWrite(MAIN_YELLOW, LOW);
-  digitalWrite(MAIN_RED, HIGH);
-  delay(100);
-  digitalWrite(PED_RED, LOW);
-  delay(100);
-  digitalWrite(PED_GREEN, HIGH);
-}
-
-void turnGreen() {
-  blinkPedRed();
-  delay(100);
-  digitalWrite(MAIN_RED, LOW);
-  digitalWrite(PED_RED, HIGH);
-  delay(200);
-  digitalWrite(MAIN_GREEN, HIGH);
-}
-
-void blinkPedRed() {
-  digitalWrite(PED_GREEN, LOW);
-
-  for (int i = 0; i < CYCLES_BLINK; i++) {
-    digitalWrite(PED_RED, HIGH);
-    delay(150);
-    digitalWrite(PED_RED, LOW);
-    delay(150);
-  }
-}
-*/
